a8677840c02c311e0887cbab6f47a13cb2d2a5d1
tonxxd <2827496+tonxxd@users.noreply.github.com>
2020-12-22 18:39:29 +0100

    only setters

--- src/plugin.js
+++ src/plugin.js
@@ -1,48 +1,50 @@
-const applyGetters = require('./applyGetters.js')
-const applySetters = require('./applySetters.js')
+const applyGetters = require("./applyGetters.js");
+const applySetters = require("./applySetters.js");
 
 const defaults = {
-  getters: {},
-  setters: {
-    applyOn: 'save',
-    // change: only execute when path was modified
-    // save: execute every time before saving
-  }
-}
+	getters: {},
+	setters: {
+		applyOn: "save",
+		// change: only execute when path was modified
+		// save: execute every time before saving
+	},
+};
 
-module.exports = exports = function plugin (schema, options) {
-  // merge defaults with options
-  options = options ? {
-    ...defaults,
-    ...options,
-    getters: {
-      ...defaults.getters,
-      ...options.getters,
-    },
-    setters: {
-      ...defaults.setters,
-      ...options.setters,
-    },
-  } : defaults
+module.exports = exports = function plugin(schema, options) {
+	// merge defaults with options
+	options = options
+		? {
+				...defaults,
+				...options,
+				getters: {
+					...defaults.getters,
+					...options.getters,
+				},
+				setters: {
+					...defaults.setters,
+					...options.setters,
+				},
+		  }
+		: defaults;
 
-  const getters = {}
-  const setters = {}
+	const getters = {};
+	const setters = {};
 
-  // extract enhanced getters and setters from schema
-  for (const id in schema.obj) {
-    const value = schema.obj[id]
+	// extract enhanced getters and setters from schema
+	for (const id in schema.obj) {
+		const value = schema.obj[id];
 
-    // path is getter
-    if (value.read instanceof Function) getters[id] = value.read
+		// path is getter
+		if (value.read instanceof Function) getters[id] = value.read;
 
-    // path is setter
-    if (value.write instanceof Function) setters[id] = value.write
-  }
+		// path is setter
+		if (value.write instanceof Function) setters[id] = value.write;
+	}
 
-  // apply getters
-  schema.post('init', applyGetters(getters, options.getters))
-  schema.post('save', applyGetters(getters, options.getters))
+	// apply getters
+	// schema.post('init', applyGetters(getters, options.getters))
+	// schema.post('save', applyGetters(getters, options.getters))
 
-  // apply setters
-  schema.pre('save', applySetters(setters, options.setters))
-}
+	// apply setters
+	schema.pre("save", applySetters(setters, options.setters));
+};
